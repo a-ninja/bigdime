a419e67755d848b30992153404d7241c771688ca
neejain <neejain@stubhub.com>
2016-09-06 15:24:40 -0700

    code refactor, unit tests

--- bigdime-commons/src/main/java/io/bigdime/core/commons/PropertyLoader.java
+++ bigdime-commons/src/main/java/io/bigdime/core/commons/PropertyLoader.java
@@ -4,16 +4,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("propertyLoader")
 public class PropertyLoader {
 
+	@Autowired(required = false)
+	private ClassLoader loader;
+
 	public Properties loadEnvProperties(String envPropertyName) throws IOException {
-		String envProperties = System.getProperty(envPropertyName);
+		String envProperties = null;
+
+		if (envPropertyName != null) {
+			envProperties = System.getProperty(envPropertyName);
+		}
+
 		System.out.println("envProperties=:" + envProperties + ":");
 		if (envProperties == null) {
 			envProperties = "application.properties";
 		}
 		System.out.println("envProperties=" + envProperties);
-		try (final InputStream is = PropertyLoader.class.getClassLoader().getResourceAsStream(envProperties)) {
+		if (loader == null) {
+			loader = PropertyLoader.class.getClassLoader();
+		}
+		try (final InputStream is = loader.getResourceAsStream(envProperties)) {
+			if (is == null) {
+				throw new IllegalArgumentException("unable to find properties:" + envProperties + ":");
+			}
 			Properties props = new Properties();
 			props.load(is);
 			return props;
